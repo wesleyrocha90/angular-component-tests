fa94d6b7521afdd040185fd05a370b6dadac91b7
wesleyrocha90 <wesleyallanrocha@gmail.com>
2020-05-04 07:53:21 -0300

    Highlight Library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  code: string = 
-`    @Component({
-      selector: 'app-root',
-      templateUrl: './app.component.html',
-      styleUrls: ['./app.component.scss']
-    })
-    export class AppComponent {
+  codeTypescript: string = 
+  `  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+  })
+  export class AppComponent {
+  }`;
+
+  codeHtml: string = 
+  `  <div *appLet="letItem as data">
+    Data: {{ data }}
+  </div>
+  <div *appLet="letItem; let data">
+    Data: {{ data }}
+  </div>`;
+
+  codeCss: string = 
+  `  @import url('https://fonts.googleapis.com/css?family=Spectral+SC:700');
+
+  body {
+    background-color: #f9f9f9;
+    h1 {
+      color: dodgerblue;
     }
-`;
+  }`;
+
+  codeJavascript: string = 
+  `  function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello there!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }`;
+
+  codeJson: string = 
+  `  {
+    "name": "ngx-highlightjs",
+    "version": "0.0.0",
+    "private": true,
+    "dependencies": { },
+    "scripts": {
+      "start": "ng serve",
+    }
+  }`;
 }
